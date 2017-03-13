4bef3ffef1cd996097f095b899b612cb2b00d801
Karol Kanicki <rw30@gazeta.pl>
2017-03-13 15:10:06 +0100

    dupa2

--- nba_2_mail.py
+++ nba_2_mail.py
@@ -6,98 +6,108 @@ from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-#SET DATES
-today = date.today()
-yesterday = today + timedelta(-2)
-#today = now.day
-#print (today)
-#print (today.year)
+def main() :
+	yesterday = setDate()
+	NBAresults = parseResults (yesterday)
+	sendMail(NBAresults)
 
-results = []
-resultsFormatted = []
+def setDate () :
+	#SET DATES
+	today = date.today()
+	yesterday = today + timedelta(-2)
+	return yesterday
 
-#SET URLs
-resultsPageURL="http://www.basketball-reference.com/boxscores/?month=" + str(yesterday.month) + "&day=" + str(yesterday.day) + "&year=" + str(yesterday.year)
+def parseResults (paramDay) :
+	#	yesterday = setDate() --------- TO BE EECUTED IN MAIN
+	results = []  # list containing pure results
+	resultsFormatted = [] # list containing formatted results, with HTML tags
 
-print (resultsPageURL)
+	#SET URLs
+	resultsPageURL="http://www.basketball-reference.com/boxscores/?month=" + str(paramDay.month) + "&day=" + str(paramDay.day) + "&year=" + str(paramDay.year)
+	#print (resultsPageURL)
 
-#Read HTML
-pageHtmlCode=urllib.request.urlopen(resultsPageURL)
-srcCode = pageHtmlCode.read()
+	#Read HTML
+	pageHtmlCode=urllib.request.urlopen(resultsPageURL)
+	srcCode = pageHtmlCode.read()
 
-parsedCode= BeautifulSoup(srcCode,"lxml")
-#parseHTML to string
-parsedCodeString=srcCode.decode("utf-8")
+	parsedCode= BeautifulSoup(srcCode,"lxml")
+	#parseHTML to string
+	parsedCodeString=srcCode.decode("utf-8")
 
-pageStartSeparator="NBA Games</h2>" #here start the table with results
-pageEndSeparator="Division Standings" # here finishes table with results
+	pageStartSeparator="NBA Games</h2>" #here start the table with results
+	pageEndSeparator="Division Standings" # here finishes table with results
 
-#removes unwanted HTML code, leaving only content between start & end separators
+	#removes unwanted HTML code, leaving only content between start & end separators
+	result2 = parsedCodeString.split(pageStartSeparator)[-1].split(pageEndSeparator)[0]
+	#again, removing some unWanted code
+	multilineString = result2.split('<div class="game_summary expanded nohover">')
 
-result2 = parsedCodeString.split(pageStartSeparator)[-1].split(pageEndSeparator)[0]
-
-#this takes parsedString and puts it to list, splitting by keyword before each cell containing result
-#so each line is actually one result:
-multilineString = result2.split('<div class="game_summary expanded nohover">')
-i=0
-
-print ('\n\n\n')
-#print (multilineString[1].split(lineStartSeparator)[-1].split(lineEndSeparator)[0] )
-for listelem in  multilineString[1:] :
-
-		homeTeamIsWinnerMarker = ""
-		visitorTeamIsWinnerMarker = ""
-	#if len(listelem) > 50 :
-		#	print (listelem.split(homeTeamStartSeparator)[-1].split(homeTeamEndSeparator)[0] + " " +  listelem.split(visitorTeamStartSeparator)[-1].split(visitorTeamEndSeparator)[0])
-		#print (listelem)
-		htmlCode=BeautifulSoup(listelem,"lxml")
-		listTeams = htmlCode.findAll('a')
-		listScores = htmlCode.findAll('td')
-		trs = htmlCode.findAll ('tr')
-		for tr in trs:
-			 if tr.has_attr('class') and tr['class'][0]=='winner':
-			 	#print (tr.td.text)
-					if tr.td.text == listTeams[2].text : #home team won game
-						homeTeamIsWinnerMarker = "*"
-					else: visitorTeamIsWinnerMarker = "*"
-		homeTeam = listTeams[2].text
-		homeTeamScore = listScores[4].text
-		visitorTeam = listTeams[0].text
-		visitorTeamScore = listScores[1].text
-
-		results.append(visitorTeam +   " @ " +  homeTeam + " " + visitorTeamScore + " : " + homeTeamScore)
-		resultsFormatted.append(visitorTeam +   " @ " +  homeTeam + " " + visitorTeamScore + " : " + homeTeamScore + "<br>")
-		if homeTeamIsWinnerMarker == "*" :
-			resultsFormatted[i] = resultsFormatted[i].replace (homeTeam, "<b>" + homeTeam + "</b>")
-			print (resultsFormatted[i])
-		else:
-			resultsFormatted[i] = resultsFormatted[i].replace (visitorTeam, "<b>" + visitorTeam + "</b>")
-		i=i+1
-
-print (*resultsFormatted, sep="\n")
-#print (results[0])
-print ('\n\n\n')
-
-### MAIL CONTENT:
-recipients=["karol.kanicki@gmail.com"]#, "inigo.hernaez@clearpeaks.com"]
-sender="cp.redmine.test@gmail.com"
-subject = "NBA Results "
-content =  ''.join(resultsFormatted)
-print (type (content))
-
-### CREATING EMAIL FROM CONTENT ABOVE
-body = MIMEText(content,'html')
-#msg = MIMEText(body)
-msg = MIMEMultipart('alternative')
-msg['Subject'] = subject
-msg['From'] = sender
-msg['To'] = ", ".join(recipients)
-msg.attach(body)
+		#this takes parsedString and puts it to list, splitting by keyword before each cell containing result
+		#so each line is actually one result:
+	i=0
 
 
-server = smtplib.SMTP ('smtp.gmail.com', 587)
-server.starttls()
-server.login (sender, "admin2000")
+	#print (multilineString[1].split(lineStartSeparator)[-1].split(lineEndSeparator)[0] )
+	for listelem in  multilineString[1:] :
 
-server.sendmail(sender, recipients, msg.as_string())
-server.quit()
+			homeTeamIsWinnerMarker = ""
+			visitorTeamIsWinnerMarker = ""
+		#if len(listelem) > 50 :
+			#	print (listelem.split(homeTeamStartSeparator)[-1].split(homeTeamEndSeparator)[0] + " " +  listelem.split(visitorTeamStartSeparator)[-1].split(visitorTeamEndSeparator)[0])
+			#print (listelem)
+			htmlCode=BeautifulSoup(listelem,"lxml")
+			listTeams = htmlCode.findAll('a')
+			listScores = htmlCode.findAll('td')
+			trs = htmlCode.findAll ('tr')
+			for tr in trs:
+				 if tr.has_attr('class') and tr['class'][0]=='winner':
+				 	#print (tr.td.text)
+						if tr.td.text == listTeams[2].text : #home team won game
+							homeTeamIsWinnerMarker = "*"
+						else: visitorTeamIsWinnerMarker = "*"
+			homeTeam = listTeams[2].text
+			homeTeamScore = listScores[4].text
+			visitorTeam = listTeams[0].text
+			visitorTeamScore = listScores[1].text
+
+			results.append(visitorTeam +   " @ " +  homeTeam + " " + visitorTeamScore + " : " + homeTeamScore)
+			resultsFormatted.append(visitorTeam +   " @ " +  homeTeam + " " + visitorTeamScore + " : " + homeTeamScore + "<br>")
+			if homeTeamIsWinnerMarker == "*" :
+				resultsFormatted[i] = resultsFormatted[i].replace (homeTeam, "<b>" + homeTeam + "</b>")
+				print (resultsFormatted[i])
+			else:
+				resultsFormatted[i] = resultsFormatted[i].replace (visitorTeam, "<b>" + visitorTeam + "</b>")
+			i=i+1
+
+	print (*resultsFormatted, sep="\n")
+	#print (results[0])
+	print ('\n')
+	return resultsFormatted
+
+def sendMail(results) :
+	recipients=["karol.kanicki@gmail.com"]#, "inigo.hernaez@clearpeaks.com"]
+	sender="cp.redmine.test@gmail.com"
+	subject = "NBA Results "
+	content =  ''.join(results)
+	print (type (content))
+
+	### CREATING EMAIL FROM CONTENT ABOVE
+	body = MIMEText(content,'html')
+	#msg = MIMEText(body)
+	msg = MIMEMultipart('alternative')
+	msg['Subject'] = subject
+	msg['From'] = sender
+	msg['To'] = ", ".join(recipients)
+	msg.attach(body)
+
+
+	server = smtplib.SMTP ('smtp.gmail.com', 587)
+	server.starttls()
+	server.login (sender, "admin2000")
+
+	server.sendmail(sender, recipients, msg.as_string())
+	server.quit()
+
+
+######################
+main()
